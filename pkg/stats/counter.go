f35a0c658ae58179966d4137f0e18a7b12a24014
Jay <jgodara@sarvika.com>
2020-05-08 20:54:35 +0530

    Added exit condition

--- pkg/stats/counter.go
+++ pkg/stats/counter.go
@@ -14,17 +14,23 @@ type statsdMetric struct {
 
 func (s statsdMetric) Increment(n string) {
 	log.Printf("Increment %s.%s", s.ns, n)
-	s.daemon.Incr(n, 1)
+	if err := s.daemon.Incr(n, 1); err != nil {
+		log.Fatalf("increment op: %s", err)
+	}
 }
 
 func (s statsdMetric) Gauge(n string, value int64) {
 	log.Printf("Gauge %s.%s at %v", s.ns, n, value)
-	s.daemon.Gauge(n, value)
+	if err := s.daemon.Gauge(n, value); err != nil {
+		log.Fatalf("gauge op: %s", err)
+	}
 }
 
 func (s statsdMetric) Time(n string, t time.Duration) {
-	log.Printf("Logged %s.%s at %dms", s.ns, n, t.Microseconds())
-	s.daemon.PrecisionTiming(n, t)
+	log.Printf("Logged %s.%s at %dms", s.ns, n, t.Milliseconds())
+	if err := s.daemon.PrecisionTiming(n, t); err != nil {
+		log.Fatalf("time op: %s", err)
+	}
 }
 
 func (s statsdMetric) Close() error {
